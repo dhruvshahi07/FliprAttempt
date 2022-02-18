a5989e4a0887097fd9fbcae5d35174f3581e126a
Dhruv Shahi <dhruv.shahi07@gmail.com>
2022-02-18 15:04:05 +0530

    attempt but failed

---
+++ app/src/main/java/com/example/fliprproject/JSONPlaceholder.java
@@ -0,0 +1,7 @@
+package com.example.fliprproject;
+
+
+public interface JSONPlaceholder {
+
+
+}
---
+++ app/src/main/java/com/example/fliprproject/Post.java
@@ -0,0 +1,27 @@
+package com.example.fliprproject;
+
+public class Post {
+
+    private String year;
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String category;
+
+    // year , category , id ,name , motivation
+
+}
